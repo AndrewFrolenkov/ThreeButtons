8c52bba92ff0251df7c44b85a193f61a5a6262c9
Andrew Frolenkov <andrewissuess@yahoo.com>
2023-05-06 11:37:02 +0300

    Initial Commit

---
+++ ThreeButtons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThreeButtons
+//
+//  Created by Андрей Фроленков on 6.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
